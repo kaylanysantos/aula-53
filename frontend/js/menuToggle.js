6ba030f0d8e5b00d619a67f99550180fd64453c0
kaylanysantos <kaylanycorrea45@gmail.com>
2024-12-06 15:23:48 -0300

    help

--- frontend/js/menuToggle.js
+++ frontend/js/menuToggle.js
@@ -4,3 +4,27 @@ const navLinks = document.querySelector(".nav-links");
 navToggle.addEventListener("click", () => {
   navLinks.classList.toggle("nav-links-active"); // Adiciona ou remove a classe que exibe o menu
 });
+
+function Cadastro () {
+  document.getElementById("name").value 
+  document.getElementById("email").value
+
+constCadastro = document.getElementById("name").value;
+constCadastro = document.getElementById("email").value;
+
+Cadastro.addEventListener("submit",function (event){
+  event.preventDefault(value);
+  validarFormulario;
+})
+}
+
+const email = "exemplo@gmail.com";
+const regex = /^[A-Za-z]{3,}$/;
+
+
+if(regex.test(email)) {
+  console.log("E-mail Válido!");
+
+} else{
+  console.log("E-mail Inválido!");
+}
